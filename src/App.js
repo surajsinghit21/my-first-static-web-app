9bc8231e9c5ca93edd1fa5cd50035f13c86a8bd8
surajsinghit21 <137288759+surajsinghit21@users.noreply.github.com>
2023-06-21 20:52:44 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
-import React from 'react';
+import logo from "./logo.svg";
+import "./App.css";
 
-function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
-}
+const App = () => {
+  const fetchData = () => {
+    fetch("https://test-static-webapp.azurewebsites.net/api/Function1")
+      .then((res) => res.json())
+      .then()
+      .catch();
+  };
+
+  return (
+    <div>
+      <button onClick={fetchData}>Fetch Data</button>
+    </div>
+  );
+};
 
 export default App;
