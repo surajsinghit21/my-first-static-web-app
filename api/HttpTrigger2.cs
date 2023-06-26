8d24c29be186f034f7e30c7970696d9e302b8439
surajsinghit21 <137288759+surajsinghit21@users.noreply.github.com>
2023-06-26 12:47:29 +0530

    Update HttpTrigger2.cs

--- api/HttpTrigger2.cs
+++ api/HttpTrigger2.cs
@@ -1,12 +1,16 @@
-using System;
-using System.IO;
-using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Extensions.Http;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace Company.Function
 {
@@ -17,9 +21,49 @@ namespace Company.Function
             [HttpTrigger(AuthorizationLevel.Anonymous, "get", "post", Route = null)] HttpRequest req,
             ILogger log)
         {
+                            ClaimsPrincipal user = AuthParse(req);
+
+                // check there's a user
+                if ((user?.Identity?.Name ?? "").Length == 0) {return new OkObjectResult("No user.");}
             string responseMessage = "Hello I am HttpTrigger2";
 
             return new OkObjectResult(responseMessage);
+        }
+
+                private class ClientPrincipal
+        {
+            public string IdentityProvider { get; set; }
+            public string UserId { get; set; }
+            public string UserDetails { get; set; }
+            public IEnumerable<string> UserRoles { get; set; }
+        }
+
+        public static ClaimsPrincipal AuthParse(HttpRequest Request)
+        {
+            var principal = new ClientPrincipal();
+
+            if (Request.Headers.TryGetValue("x-ms-client-principal", out var header))
+            {
+                var data = header[0];
+                var decoded = Convert.FromBase64String(data);
+                var json = Encoding.UTF8.GetString(decoded);
+                principal = System.Text.Json.JsonSerializer.Deserialize<ClientPrincipal>(json, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
+
+            }
+
+            principal.UserRoles = principal.UserRoles?.Except(new string[] { "anonymous" }, StringComparer.CurrentCultureIgnoreCase);
+
+            if (!principal.UserRoles?.Any() ?? true)
+            {
+                return new ClaimsPrincipal();
+            }
+
+            var identity = new ClaimsIdentity(principal.IdentityProvider);
+            identity.AddClaim(new Claim(ClaimTypes.NameIdentifier, principal.UserId));
+            identity.AddClaim(new Claim(ClaimTypes.Name, principal.UserDetails));
+            identity.AddClaims(principal.UserRoles.Select(r => new Claim(ClaimTypes.Role, r)));
+
+            return new ClaimsPrincipal(identity);
         }
     }
 }
